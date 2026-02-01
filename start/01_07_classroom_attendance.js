d746b26b825a8e5e6848d8481d17ec4488e33715
Poria Jeet K. <poriajeet183@gmail.com>
2026-02-01 13:35:48 +0000

    destructuring

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,22 @@
-// Write your code here
+function getStudent(classRoom) {
+  let { hasTechingAssistant, classList } = classRoom;
+  let teacher, teachingAssistant, students;
+
+  if(hasTechingAssistant) {
+    [teacher, teachingAssistant, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+  
+  // let last = [...classList];
+  // console.log(`Last :: `, last.pop());
+  return students;
+}
+
+
+const classRoom = {
+  hasTechingAssistant: false,
+  classList : ["John", "Larry", "Elon", "Tim", "Sundar"]
+}
+
+console.log(getStudent(classRoom));
