6a8e187174ed6ba641ac2f6208035a678faab4b3
Poria Jeet K. <poriajeet183@gmail.com>
2026-02-03 09:57:52 +0000

    Array reduce method

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,19 @@
-// Write your code here
+function coffeeDate(coffeeArr) {
+  let coffeeSum = coffeeArr.reduce(
+    (totalCoffees, numCoffees) => (totalCoffees += numCoffees)
+  );
+
+  return `The total bill is ${coffeeSum}`;
+}
+
+console.log(coffeeDate([4,4,4,4,4,4]))
+
+
+
+let Marks = [89,78,99,87,73,92];
+
+let max = Marks.reduce((current, next) => {
+  return Math.max(current, next);
+})
+
+console.log(`Max : ${max}`)
